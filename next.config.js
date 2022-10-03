27c32e3d3c5f142ffce6231805628e3c417d7fcc
Lance Ellis <scdelance@gmail.com>
2022-10-03 01:06:15 -0500

    removed unnecc packages

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 768, 1024, 1280, 1536],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
