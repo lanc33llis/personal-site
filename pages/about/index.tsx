3b6d4284ba701e22958d2dae8a7afda5620b1993
Lance Ellis <scdelance@gmail.com>
2022-12-31 00:00:01 -0600

    more about content

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -12,41 +12,52 @@ interface AboutDetailLine {
   right: string;
 }
 
-const workExperiences: AboutDetailLine[] = [
-  {
-    first: "Amazon",
-    middle: "Software Development Engineer Intern",
-    right: "Summer 2022",
-  },
-  {
-    first: "Major League Hacking",
-    middle: "Production Engineering Fellow",
-    right: "Summer 2021",
-  },
-  {
-    first: "Trinity University",
-    middle: "Computer Science Research Intern",
-    right: "2020 - 2021",
-  },
-];
+type AboutSection = { title: string; lines: AboutDetailLine[] };
 
-const skills: AboutDetailLine[] = [
-  {
-    first: "Web Development",
-    middle: "React, Next.js, TailwindCSS, Django, Spring, Express",
-    right: "2 years",
-  },
-  {
-    first: "DevOps",
-    middle: "Docker, AWS, Bash Scripting, GitHub Actions",
-    right: "2 years",
-  },
-  {
-    first: "Languages",
-    middle: "Python, Java, C++, JavaScript/TypeScript",
-    right: "",
-  },
-];
+const workExperiences: AboutSection = {
+  title: "Experiences",
+  lines: [
+    {
+      first: "Amazon",
+      middle: "Software Development Engineer Intern",
+      right: "Summer 2022",
+    },
+    {
+      first: "Major League Hacking",
+      middle: "Production Engineering Fellow",
+      right: "Summer 2021",
+    },
+    {
+      first: "Trinity University",
+      middle: "Computer Science Research Intern",
+      right: "2020 - 2021",
+    },
+  ],
+};
+
+const skills: AboutSection = {
+  title: "Skills",
+  lines: [
+    {
+      first: "Web Development",
+      middle: "React, Next.js, TailwindCSS, Django, Spring, Express",
+      right: "2 years",
+    },
+    {
+      first: "DevOps",
+      middle: "Docker, AWS, Bash Scripting, GitHub Actions",
+      right: "2 years",
+    },
+    {
+      first: "Languages",
+      middle: "Python, Java, C++, JavaScript/TypeScript",
+      right: "",
+    },
+  ],
+};
+
+const sections: AboutSection[] = [workExperiences, skills];
+
 const About: NextPage = () => {
   return (
     <div>
@@ -55,14 +66,14 @@ const About: NextPage = () => {
       </Head>
       <Header forceOpen />
       <motion.div
-        className="absolute top-0 flex flex-col items-center justify-center px-8 pt-16 pb-28 md:pb-12 lg:pb-0 sm:px-16 lg:px-24"
+        className="absolute top-0 flex flex-col items-center justify-center px-8 sm:px-16 lg:px-24"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ type: "tween", duration: 0.7 }}
       >
         <div className="flex flex-col w-full ">
-          <div className="flex flex-col justify-center w-full min-h-screen gap-4 md:gap-8 lg:gap-16">
+          <div className="flex flex-col justify-center w-full min-h-screen gap-4 pt-16 md:gap-8 lg:gap-16 pb-28 md:pb-12 lg:pb-0">
             <h2 className="text-2xl font-medium md:text-3xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
               Hi! I&apos;m Lance, a web developer and <br /> designer in Austin,
               TX.
@@ -84,47 +95,42 @@ const About: NextPage = () => {
           </div>
 
           <div>
-            <div className="flex flex-col w-full mb-16">
-              <h2 className="py-4 text-4xl font-semibold border-black border-b-[1.5px]">
-                Experiences
-              </h2>
-              {workExperiences.map((we) => (
-                <div
-                  className="flex py-4 border-black border-b-[1.5px]"
-                  key={we.first}
-                >
-                  <div className="w-48 mr-32">
-                    <h3 className="text-lg">{we.first}</h3>
-                  </div>
-                  <div className="mr-32 w-[500px]">
-                    <h3 className="text-lg">{we.middle}</h3>
-                  </div>
-                  <div className="w-32 mr-32">
-                    <h3 className="text-lg">{we.right}</h3>
-                  </div>
-                </div>
-              ))}
-              <h2 className="py-4 mt-16 text-4xl font-semibold border-black border-b-[1.5px]">
-                Skills
-              </h2>
-              {skills.map((s) => (
-                <div
-                  className="flex py-4 border-black border-b-[1.5px]"
-                  key={s.first}
-                >
-                  <div className="w-48 mr-32">
-                    <h3 className="text-lg">{s.first}</h3>
-                  </div>
-                  <div className="mr-32 w-[500px]">
-                    <h3 className="text-lg">{s.middle}</h3>
-                  </div>
-                  <div className="w-32 mr-32">
-                    <h3 className="text-lg">{s.right}</h3>
-                  </div>
+            <div className="flex flex-col w-full ">
+              {sections.map((s) => (
+                <div key={s.title} className="mb-8 lg:mb-16">
+                  <h2 className="py-4 text-2xl font-semibold border-black md:text-3xl: lg:text-4xl border-b-[1.5px]">
+                    {s.title}
+                  </h2>
+                  {s.lines.map((l) => (
+                    <div
+                      className="flex py-4 border-black border-b-[1.5px]"
+                      key={l.first}
+                    >
+                      <div className="w-[6.5rem] md:w-48">
+                        <h3 className="text-sm md:text-base lg:text-lg">
+                          {l.first}
+                        </h3>
+                      </div>
+                      <div className="flex flex-col gap-4 md:flex-row">
+                        <div className="w-[200px] md:w-[300px] lg:w-[400px]">
+                          <h3 className="text-sm md:text-base lg:text-lg">
+                            {l.middle}
+                          </h3>
+                        </div>
+                        <div className="w-32 ">
+                          <h3 className="text-sm md:text-base lg:text-lg">
+                            {l.right}
+                          </h3>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               ))}
             </div>
-            <Footer marginY={false} />
+            <div className="mt-4 mb-12 lg:mb-16">
+              <Footer marginY={false} />
+            </div>
           </div>
         </div>
       </motion.div>
