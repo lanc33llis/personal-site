e677f36df6bb5f260c9bd9833ee5f96863b600d0
Lance Ellis <scdelance@gmail.com>
2024-02-01 22:36:45 -0600

    minor footer changes

--- pages/components/Footer/index.tsx
+++ pages/components/Footer/index.tsx
@@ -6,28 +6,24 @@ const Footer = () => {
     <div
       className={`flex select-none leading-none text-zinc-400 flex-col mt-24 pt-12 pb-4 sm:pb-16 lg:pb-24 w-full mb-24 px-4 sm:px-16 lg:px-24`}
     >
-      <div>
-        <span className="inline-block select-text w-[600px]">Lance Ellis</span>
-        <div className="inline-block w-[600px]">
-          <Link
-            href="https://www.linkedin.com/in/lance-ellis"
-            className="hover:text-black transition-colors"
-          >
-            LinkedIn
-          </Link>
-        </div>
-        <span>Austin, 2024</span>
+      <div className="flex justify-between">
+        <span className="select-text">Lance Ellis</span>
+
+        <Link
+          href="https://www.linkedin.com/in/lance-ellis"
+          className="hover:text-black transition-colors"
+        >
+          LinkedIn
+        </Link>
       </div>
-      <div>
-        <span className="inline-block select-text w-[600px]">© 2024</span>
-        <div className="inline-block w-[600px]">
-          <Link
-            href="https://github.com/lanc33llis"
-            className="hover:text-black transition-colors"
-          >
-            GitHub
-          </Link>
-        </div>
+      <div className="flex justify-between">
+        <span>© 2024</span>
+        <Link
+          href="https://github.com/lanc33llis"
+          className="hover:text-black transition-colors"
+        >
+          GitHub
+        </Link>
       </div>
     </div>
   );
