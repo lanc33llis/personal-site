2a97a3266139d467a43cd22965b093320f5c632a
Lance Ellis <scdelance@gmail.com>
2025-01-06 18:44:53 -0600

    mostly grammar

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -6,12 +6,17 @@ import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 
 import hyperlineLight from "@/styles/hyperline-light";
 import { ArrowDownIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import MarkdownIcon from "./markdown-icon";
+import { cn } from "@/lib/utils";
 
-const snippet = `Hey! I am a self-taught programmer specializing in 
-web development and design. I'm deeply proficient with TypeScript,
-React, and Next.js. I enjoy building large scaling applications;
-I have a heavy background on DevOps and CI/CD pipelines. Recently,
-I've been building web applications and libraries for LLMs. 
+const files = {
+  "README.md": {
+    lastEdit: "1/6/25 6:44PM CST",
+    content: `Hey! I am a self-taught programmer specializing in web development 
+and design. I'm highly proficient with TypeScript, React, and 
+Next.js. I enjoy building large scaling applications; I have a 
+strong background in DevOps and CI/CD pipelines. Recently, I've 
+been focusing on building web applications and libraries for LLMs. 
 
 I've worked for Amazon, where I developed internal tools and later,
 user-facing features for the Alexa app. My experience also includes 
@@ -20,7 +25,19 @@ and web development. I've also worked previously at Atlassian on
 their governance security team. In addition, I took a fellowship at
 Major League Hacking to learn about production engineering.
 
-I'll be returning to Atlassian as a software engineer intern.`;
+I'll be returning to Atlassian as a software engineer intern.`,
+  },
+  "RESUME.md": {
+    lastEdit: "12/8/24 11:53PM CST",
+    content: `# Lance Ellis`,
+  },
+} as const satisfies Record<
+  string,
+  {
+    lastEdit: string;
+    content: string;
+  }
+>;
 
 const About = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -32,137 +49,54 @@ const About = () => {
 
   const codeRef = useRef<HTMLPreElement>(null);
 
+  const [selectedFile, setSelectedFile] =
+    useState<keyof typeof files>("README.md");
+
+
   return (
     <div
       ref={ref}
       className="relative h-screen flex justify-center snap-center items-center overflow-hidden"
     >
-      {/* <div className="w-[200px] lg:w-[300px] h-fit absolute">
-        <m.div
-          initial={{ opacity: 0, y: "var(--y-from)", x: "var(--x)" }}
-          {...(inView && {
-            initial: { opacity: 0, y: "var(--y-from)", x: "var(--x)" },
-            animate: { opacity: 1, y: "var(--y-to)" },
-          })}
-          transition={{ duration: 2, type: "spring" }}
-          className="
-            [--x:-85px] 
-            sm:[--x:-205px] 
-            md:[--x:-305px] 
-            [--y-from:-240px]
-            [--y-to:-140px]
-            sm:[--y-from:-220px] 
-            sm:[--y-to:-70px]
-          "
-        >
-          <Image
-            style={{ clipPath: "url(#computer-image)" }}
-            width={0.8}
-            height={1}
-            layout="responsive"
-            src={"/andras-vas-Bd7gNnWJBkU-unsplash.jpg"}
-            alt="picture of me by Rae Xin"
-            quality={10}
-            priority
-          />
-          <svg className="absolute left-0 right-0 w-full h-full">
-            <defs>
-              <clipPath id="computer-image">
-                <m.rect
-                  {...(inView && {
-                    initial: { y: "0", opacity: 0 },
-                    animate: { y: "5%", opacity: 1 },
-                  })}
-                  transition={{ duration: 3, type: "spring" }}
-                  x="15%"
-                  width="70%"
-                  height="95%"
-                  rx={10}
-                />
-              </clipPath>
-            </defs>
-          </svg>
-        </m.div>
-      </div>
-      <div className="w-[200px] lg:w-[300px] h-fit absolute">
-        <m.div
-          transition={{ duration: 2, type: "spring", delay: 0.5 }}
-          initial={{ opacity: 0, y: "var(--y-from)", x: "var(--x)" }}
-          {...(inView && {
-            initial: { opacity: 0, y: "var(--y-from)", x: "var(--x)" },
-            animate: { opacity: 1, y: "var(--y-to)" },
-          })}
-          className="
-            [--x:85px] 
-            sm:[--x:205px]
-            md:[--x:305px] 
-            [--y-from:40px]
-            [--y-to:140px]
-            md:[--y-from:-80px] 
-            md:[--y-to:60px]
-          "
-        >
-          <Image
-            style={{ clipPath: "url(#space-image)" }}
-            width={0.7913232}
-            height={1}
-            layout="responsive"
-            src={"/joel-filipe-QwoNAhbmLLo-unsplash.jpg"}
-            alt="picture of me by Rae Xin"
-            quality={10}
-            priority
-          />
-          <svg className="absolute left-0 right-0 w-full h-full">
-            <defs>
-              <clipPath id="space-image">
-                <m.rect
-                  {...(inView && {
-                    initial: { y: "0", opacity: 0 },
-                    animate: { y: "5%", opacity: 1 },
-                  })}
-                  transition={{ duration: 3, type: "spring", delay: 0.5 }}
-                  x="15%"
-                  width="70%"
-                  height="95%"
-                  rx={10}
-                />
-              </clipPath>
-            </defs>
-          </svg>
-        </m.div>
-      </div> */}
-      <div className="relative w-fit h-max flex flex-col gap-1">
-        {/* WIP File Explorer for different 'files' */}
-        {/* <div className="text-xs absolute z-50 -translate-x-[150%] flex flex-col h-full p-2 rounded border bg-background/25 border-background/25 dark:border-foreground/10 dark:bg-foreground/10 shadow backdrop-blur-xl">
-          <span className="flex gap-1 items-center pb-2">
+      <div className="relative max-w-[622px] h-max flex flex-col gap-1">
+        {/* <div className="text-xs absolute z-50 -translate-x-[150%] flex flex-col h-full rounded border bg-background/25 border-background/25 dark:border-foreground/10 dark:bg-foreground/10 shadow backdrop-blur-xl">
+          <span className="flex gap-1 items-center p-2">
             <ChevronDownIcon />
             <span>lancee</span>
           </span>
-          <button>
-            <span>README.md</span>
-          </button>
+          {Object.keys(files).map((file) => (
+            <button
+              key={file}
+              className={cn(
+                "flex gap-1 px-2 hover:bg-foreground/10 py-0.5",
+                file === selectedFile && "bg-foreground/10 text-foreground/100"
+              )}
+              onClick={() => setSelectedFile(file as keyof typeof files)}
+            >
+              <MarkdownIcon />
+              <span>{file}</span>
+            </button>
+          ))}
         </div> */}
-        <div className="mt-2 w-fit flex flex-col rounded border bg-background/25 border-background/25 dark:border-foreground/10 dark:bg-foreground/10 shadow backdrop-blur-xl">
-          <div className="border-b flex justify-between border-background/25 dark:border-foreground/10 p-1 sm:p-2 pl-2 text-left font-code text-[8px] sm:text-xs">
-            <span>README.md</span>
-            <span className="dark:text-foreground">12/8/24 11:53PM CST</span>
+        <div className="w-fit flex my-4 flex-col rounded border bg-background/25 border-background/25 dark:border-foreground/10 dark:bg-foreground/10 shadow backdrop-blur-xl">
+          <div className="border-b flex justify-between border-background/25 dark:border-foreground/10 p-1 sm:p-2 pl-2 text-left font-code text-[.5rem] sm:text-xs">
+            <span>{selectedFile}</span>
+            <span className="dark:text-foreground">
+              {files[selectedFile].lastEdit}
+            </span>
           </div>
-          <pre className="flex h-full p-1 sm:p-2 text-[8px] sm:text-sm w-fit">
+          <pre className="flex h-full p-1 sm:p-2 text-[8px] sm:text-xs md:text-sm w-fit">
             <div className="h-full flex flex-col pr-4">
-              {Array.from({ length: 14 }, (_, i) => i + 1).map((line) => (
+              {Array.from({ length: files[selectedFile].content.split("\n").length }, (_, i) => i + 1).map((line) => (
                 <span key={line} className="text-end dark:text-foreground">
                   {line}
                 </span>
               ))}
             </div>
             <code className="dark:text-foreground" ref={codeRef}>
-              {snippet}
+              {files[selectedFile].content}
             </code>
           </pre>
-        </div>
-        <div className="w-full flex items-center gap-1">
-          <ArrowDownIcon />
-          <span>Explore my work</span>
         </div>
       </div>
     </div>
---
+++ components/Home/markdown-icon.tsx
@@ -0,0 +1,16 @@
+const MarkdownIcon = () => {
+  return (
+    <svg
+      width="25"
+      fill="none"
+      viewBox="0 0 208 128"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g className="fill-foreground">
+        <path d="m30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zm125 0-30-33h20v-35h20v35h20z" />
+      </g>
+    </svg>
+  );
+};
+
+export default MarkdownIcon;
